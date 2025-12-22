0643e141d998d50f043872084cbe5f12f9770394
abdullahalsalman44 <alsalmanabd11@gmail.com>
2025-12-23 01:20:27 +0300

    feat(Security) Create Proxy Instance for check user role before make action

--- src/accounts/AccountFactory.java
+++ src/accounts/AccountFactory.java
@@ -1,24 +1,27 @@
 package accounts;
 
+import security.User;
+
 public class AccountFactory {
 
     public static Account create(AccountType type,
                                  String id,
-                                 double balance) {
+                                 double balance,
+                                 User user) {
 
         switch (type) {
 
             case SAVING:
-                return new SavingAccount(id, balance, 1000, 0.03);
+                return new ProxyAbstractAccount(new SavingAccount(id, balance, 1000, 0.03) , user);
 
             case CHECKING:
-                return new CheckingAccount(id, balance, 2000);
+                return new ProxyAbstractAccount(new CheckingAccount(id, balance, 2000) , user)
 
             case LOAN:
-                return new LoanAccount(id, balance, 0.05);
+                return new ProxyAbstractAccount(new LoanAccount(id, balance, 0.05) , user) ;
 
             case INVESTMENT:
-                return new InvestmentAccount(id, balance, 0.02);
+                return new ProxyAbstractAccount(new InvestmentAccount(id, balance, 0.02) , user) ;
 
             default:
                 throw new IllegalArgumentException("Unknown account type");
